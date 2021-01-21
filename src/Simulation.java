e01fed4c8e92b2ec68445b8f88ac22cf9d6fe96f
Coleman Rose <colemanrose.new@gmail.com>
2021-01-21 11:31:56 +0100

    incremental improvement.

--- src/Simulation.java
+++ src/Simulation.java
@@ -5,12 +5,14 @@ public class Simulation {
 	int height;
 	int [] [] board;
 	
+	// Constructor for Simulation class
 	public Simulation(int width, int height) {
 		this.width = width;
 		this.height = height;
 		this.board = new int[width][height];
 	}
 	
+	// Prints board with the given height and width using nested for loops.
 	public void printBoard() {
 		System.out.println("---");
 		for (int y = 0; y < height; y++) {
@@ -28,12 +30,49 @@ public class Simulation {
 		System.out.println("---\n");
 	}
 	
+	// set the alive spaces in the initial state using coordinates.
 	public void setAlive(int x, int y) {
 		this.board[x][y] = 1;
 	}
 	
+	// same except for dead spaces
 	public void setDead(int x, int y) {
 		this.board[x][y] = 0;
+	}
+	
+	// Counts the amount of squares around the current square that are alive. 
+	public int countAliveNeighbors(int x, int y) {
+		int count = 0;
+		
+		count += this.board[x - 1][y - 1];
+		count += this.board[x][y - 1];
+		count += this.board[x + 1][y - 1];
+		
+		count += this.board[x - 1][y];
+		count += this.board[x + 1][y];
+		
+		count += this.board[x - 1][y + 1];
+		count += this.board[x][y - 1];
+		count += this.board[x + 1][y + 1];
+		
+		return count;
+	}
+	
+	public void step() {
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				int aliveNeighbors = countAliveNeighbors(x, y);
+				
+				if (this.board[x][y] == 1) {
+					if (aliveNeighbors < 2) {
+						this.board[x][y] = 0;
+					}
+				} else {
+					
+				}
+				
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
@@ -44,5 +83,7 @@ public class Simulation {
 		simulation.setAlive(4, 2);
 		
 		simulation.printBoard();
+		
+		System.out.println(simulation.countAliveNeighbors(3, 2));
 	}
 }
